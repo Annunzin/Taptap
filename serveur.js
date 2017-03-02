6f2ed0ed5f3100e5d0030534dc7dff763448c7b1
Jérémie Moutinho <moutinhj@pc-b123-01.iut-valence.fr>
2017-03-02 14:38:56 +0100

    MAJ, resolution pb

--- serveur.js
+++ serveur.js
@@ -27,12 +27,37 @@ var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
     console.log('Un client est connecté !');
+    
+   
+    
     socket.emit('message', 'Vous êtes bien connecté, la partie peut commencer !');
+    
     socket.on('clickBtn', function (bouton) {
         console.log('Un bouton a été préssé : le ' + message);
     }); 
     socket.on('newPlayer', function (joueur) {
         console.log('Le joueur ' + joueur + ' a rejoinds la partie !');
+        
+         serialPort = new SerialPort('COM3', { //portName ?
+            baudrate: 9600,
+            // defaults for Arduino serial communication
+             dataBits: 8, 
+             parity: 'none', 
+             stopBits: 1, 
+             flowControl: false 
+        });
+        serialPort.on("open", function () {
+            console.log('open serial communication');
+            // Listens to incoming data
+            serialPort.on('btnPress', function(btnID) { 
+                console.log("Button " + btnID + " pressed");
+                if (btnID > 0) {
+                // save the data between 'B' and 'E'
+                socketServer.emit('updateScore', 'ZOB' );
+                }
+            });  
+        }); 
+        
         socket.emit('addPlayer', joueur);
     }); 
 });
